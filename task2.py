54dc087cb8a622844119c63172c807a96cca582f
Tasha Natasha <0955712824n@gmail.com>
2025-09-24 20:35:38 +0300

    fix task2

--- task2.py
+++ task2.py
@@ -5,15 +5,15 @@ def get_numbers_ticket(min: int, max: int, quantity: int):
     Generates a set of unique random numbers
     """
 
-    min_value = 1 if min < 1 else min
-    max_value = 1000 if max > 1000 else max
-
-    if quantity > (max_value - min_value + 1):
+    if quantity > (max - min + 1)  or  min < 1 or  max > 1000 :
           return []
 
     result_array = set()
     while len(result_array) != quantity:
-        result_array.add(randint(min_value, max_value))
+        result_array.add(randint(min, max))
 
     result = sorted(list(result_array))
     return result
+
+
+# print(get_numbers_ticket(-10, 10, 5))
